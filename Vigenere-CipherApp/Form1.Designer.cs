6471669ce99f24111817a2d49f7846f7133a3f5b
F55121082_FEBRIYADI <andika.saputra2402@gmail.com>
2024-04-02 00:10:09 +0800

    Add project files.

---
+++ Vigenere-CipherApp/Form1.Designer.cs
@@ -0,0 +1,204 @@
+﻿namespace Vigenere_CipherApp
+{
+    partial class MainThread
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.buttonBrowse = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.buttonStart = new System.Windows.Forms.Button();
+            this.buttonStop = new System.Windows.Forms.Button();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.buttonTambah = new System.Windows.Forms.Button();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // buttonBrowse
+            // 
+            this.buttonBrowse.Location = new System.Drawing.Point(917, 241);
+            this.buttonBrowse.Name = "buttonBrowse";
+            this.buttonBrowse.Size = new System.Drawing.Size(99, 20);
+            this.buttonBrowse.TabIndex = 0;
+            this.buttonBrowse.Text = "Upload Plain Text";
+            this.buttonBrowse.UseVisualStyleBackColor = true;
+            this.buttonBrowse.Click += new System.EventHandler(this.buttonBrowse_Click_1);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(382, 241);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(517, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(382, 137);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox2.Size = new System.Drawing.Size(634, 88);
+            this.textBox2.TabIndex = 2;
+            this.textBox2.Text = "Plain Text . . .";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(382, 281);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.ReadOnly = true;
+            this.textBox3.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox3.Size = new System.Drawing.Size(634, 82);
+            this.textBox3.TabIndex = 3;
+            this.textBox3.Text = "Key Yang di Perpanjang";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(382, 377);
+            this.textBox4.Multiline = true;
+            this.textBox4.Name = "textBox4";
+            this.textBox4.ReadOnly = true;
+            this.textBox4.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox4.Size = new System.Drawing.Size(634, 110);
+            this.textBox4.TabIndex = 4;
+            this.textBox4.Text = "Hasil  atau chipertext";
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(382, 606);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(530, 32);
+            this.progressBar1.TabIndex = 5;
+            // 
+            // buttonStart
+            // 
+            this.buttonStart.Location = new System.Drawing.Point(382, 666);
+            this.buttonStart.Name = "buttonStart";
+            this.buttonStart.Size = new System.Drawing.Size(239, 36);
+            this.buttonStart.TabIndex = 6;
+            this.buttonStart.Text = "Start";
+            this.buttonStart.UseVisualStyleBackColor = true;
+            this.buttonStart.Click += new System.EventHandler(this.buttonStart_Click);
+            // 
+            // buttonStop
+            // 
+            this.buttonStop.Location = new System.Drawing.Point(835, 666);
+            this.buttonStop.Name = "buttonStop";
+            this.buttonStop.Size = new System.Drawing.Size(181, 36);
+            this.buttonStop.TabIndex = 8;
+            this.buttonStop.Text = "Stop";
+            this.buttonStop.UseVisualStyleBackColor = true;
+            this.buttonStop.Click += new System.EventHandler(this.buttonStop_Click);
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(382, 503);
+            this.textBox5.Multiline = true;
+            this.textBox5.Name = "textBox5";
+            this.textBox5.ReadOnly = true;
+            this.textBox5.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox5.Size = new System.Drawing.Size(634, 85);
+            this.textBox5.TabIndex = 9;
+            this.textBox5.Text = "Hasil Chipertext ke-2";
+            // 
+            // buttonTambah
+            // 
+            this.buttonTambah.Location = new System.Drawing.Point(627, 666);
+            this.buttonTambah.Name = "buttonTambah";
+            this.buttonTambah.Size = new System.Drawing.Size(202, 36);
+            this.buttonTambah.TabIndex = 10;
+            this.buttonTambah.Text = "New";
+            this.buttonTambah.UseVisualStyleBackColor = true;
+            this.buttonTambah.Click += new System.EventHandler(this.buttonTambah_Click);
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(930, 606);
+            this.textBox6.Multiline = true;
+            this.textBox6.Name = "textBox6";
+            this.textBox6.ReadOnly = true;
+            this.textBox6.Size = new System.Drawing.Size(86, 32);
+            this.textBox6.TabIndex = 11;
+            this.textBox6.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // MainThread
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1329, 722);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.buttonTambah);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.buttonStop);
+            this.Controls.Add(this.buttonStart);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.buttonBrowse);
+            this.Name = "MainThread";
+            this.Text = "Vigenere Cipher Application";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button buttonBrowse;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Button buttonStart;
+        private System.Windows.Forms.Button buttonStop;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Button buttonTambah;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+    }
+}
+
---
+++ Vigenere-CipherApp/Form1.cs
@@ -0,0 +1,191 @@
+﻿using System;
+using System.ComponentModel;
+using System.IO;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace Vigenere_CipherApp
+{
+    public partial class MainThread : Form
+    {
+        BackgroundWorker worker;
+        string plaintext = "";
+        string key = "";
+        Thread slaveThread;
+
+        public MainThread()
+        {
+            InitializeComponent();
+            InitializeBackgroundWorker();
+        }
+
+        private void InitializeBackgroundWorker()
+        {
+            worker = new BackgroundWorker();
+            worker.WorkerReportsProgress = true;
+            worker.WorkerSupportsCancellation = true;
+            worker.DoWork += Worker_DoWork;
+            worker.ProgressChanged += Worker_ProgressChanged;
+            worker.RunWorkerCompleted += Worker_RunWorkerCompleted;
+        }
+
+        private void EncryptVigenere(string plaintext, string key, BackgroundWorker bgWorker)
+        {
+            int keyIndex = 0;
+            int keyLength = key.Length;
+
+            StringBuilder encryptedText = new StringBuilder();
+
+            for (int i = 0; i < plaintext.Length; i++)
+            {
+                if (bgWorker.CancellationPending)
+                {
+                    return;
+                }
+
+                char plainChar = plaintext[i];
+                char keyChar = key[keyIndex];
+                char encryptedChar = (char)(((plainChar + keyChar) % 26) + 'A');
+
+                encryptedText.Append(encryptedChar);
+
+                keyIndex = (keyIndex + 1) % keyLength;
+
+                int progressPercentage = (i + 1) * 100 / plaintext.Length;
+                bgWorker.ReportProgress(progressPercentage, encryptedText.ToString());
+
+                // Menjalankan operasi slave thread secara eksplisit
+                if (i == plaintext.Length / 2)
+                {
+                    slaveThread = new Thread(SlaveThreadFunction);
+                    slaveThread.Start();
+                }
+
+                // Simulasi tugas yang memakan waktu
+                Thread.Sleep(100);
+            }
+
+            // Mengembalikan hasil enkripsi
+            bgWorker.ReportProgress(100, encryptedText.ToString());
+        }
+
+        private void Worker_DoWork(object sender, DoWorkEventArgs e)
+        {
+            BackgroundWorker bgWorker = sender as BackgroundWorker;
+            EncryptVigenere(plaintext, key, bgWorker);
+        }
+
+        private void Worker_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            progressBar1.Value = e.ProgressPercentage;
+            progressBar1.Step = 1; // Tentukan ukuran langkah kemajuan
+            progressBar1.PerformStep();
+            textBox4.Text = e.UserState as string;
+            textBox6.Text = $"{progressBar1.Value}% Complete";
+        }
+
+        private void Worker_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            if (e.Cancelled)
+            {
+                MessageBox.Show("Operation was cancelled.");
+            }
+            else if (e.Error != null)
+            {
+                MessageBox.Show("An error occurred: " + e.Error.Message);
+            }
+        }
+        private void SlaveThreadFunction()
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine($"Slave Thread: Iteration {i}");
+                Thread.Sleep(1000);
+            }
+        }
+
+        // Metode untuk tombol Start
+        private void buttonStart_Click(object sender, EventArgs e)
+        {
+            if (!worker.IsBusy)
+            {
+                key = textBox1.Text;
+                progressBar1.Value = 0;
+
+                if (key.Length < plaintext.Length)
+                {
+                    key = ExtendKey(key);
+                }
+
+                textBox3.Text = key;
+                worker.RunWorkerAsync();
+            }
+        }
+        private void buttonBrowse_Click_1(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Text Files (*.txt)|*.txt|All Files (*.*)|*.*";
+            openFileDialog.FilterIndex = 1;
+            openFileDialog.RestoreDirectory = true;
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    plaintext = File.ReadAllText(openFileDialog.FileName);
+                    textBox2.Text = plaintext;
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error: Could not read file from disk. Original error: " + ex.Message);
+                }
+            }
+        }
+        private void buttonStop_Click(object sender, EventArgs e)
+        {
+            if (worker.IsBusy)
+            {
+                worker.CancelAsync();
+            }
+            // Menghentikan slave thread jika sedang berjalan
+            if (slaveThread != null && slaveThread.IsAlive)
+            {
+                slaveThread.Abort();
+            }
+        }
+
+        // Metode untuk menambahkan kunci baru
+        private void buttonTambah_Click(object sender, EventArgs e)
+        {
+            // Ambil teks asli dari textBox2 dan kunci baru dari textBox1
+            string newKey = textBox1.Text;
+
+            // Lakukan enkripsi ulang dengan kunci baru
+            StringBuilder reencryptedText = new StringBuilder();
+            int keyIndex = 0;
+            int keyLength = newKey.Length;
+
+            for (int i = 0; i < plaintext.Length; i++)
+            {
+                char plainChar = plaintext[i];
+                char keyChar = newKey[keyIndex];
+                char encryptedChar = (char)(((plainChar + keyChar) % 26) + 'A');
+                reencryptedText.Append(encryptedChar);
+                keyIndex = (keyIndex + 1) % keyLength;
+            }
+
+            // Tampilkan chipertext hasil enkripsi ulang di textBox5
+            textBox5.Text = reencryptedText.ToString();
+        }
+        private string ExtendKey(string key)
+        {
+            StringBuilder extendedKey = new StringBuilder();
+            for (int i = 0; i < plaintext.Length; i++)
+            {
+                extendedKey.Append(key[i % key.Length]);
+            }
+            return extendedKey.ToString();
+        }
+    }
+}
